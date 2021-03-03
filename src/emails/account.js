b7408372b12699b763f1a1348051913dd49bc9b8
meetp24 <meet.patel1@bacancy.com>
2021-03-03 15:17:43 +0530

    Init Commit

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,17 +3,6 @@ const DOMAIN = "sandbox9b6d2848135a414c821b9520672eadce.mailgun.org";
 //const mg = mailgun({apiKey: "b007dab23af757780dab7000b018640c-e49cc42c-14bed54a", domain: DOMAIN});
 const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN});
 
-// const data = {
-// 	from: "Mailgun Sandbox <postmaster@sandbox9b6d2848135a414c821b9520672eadce.mailgun.org>",
-// 	to: "meet.https@gmail.com",
-// 	subject: "Selected in Apple",
-// 	text: "You are selected in Apple.Upon Successful completion of your degree, you will be on board as Lead Software Engineer with an annual remuneration of 50 CPA."
-// };
-
-// mg.messages().send(data, function (error, body) {
-//     console.log(body);
-// });
-
 const sendWelcomeEmail = (email, name) => {
 	mg.messages().send ({ 
 		to: email,
